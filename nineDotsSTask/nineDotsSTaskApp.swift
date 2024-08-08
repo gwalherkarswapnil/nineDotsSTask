42e28a66e2d55e48d1c698daee6160cfaabc5f19
Swapnil <gwalherkarswapnil@gmail.com>
2024-08-08 19:00:18 +0530

    Initial Commit

---
+++ nineDotsSTask/nineDotsSTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  nineDotsSTaskApp.swift
+//  nineDotsSTask
+//
+//  Created by Swapnil on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct nineDotsSTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
